734558dc4d8b0c746791b1df0ced51ccee869ed2
Samuthe <ekatisamuel@gmail.com>
2023-02-20 11:24:14 +0300

    return start and end index

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/en python3
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """return start and end of index"""
+    starting = (page - 1) * page_size
+    end = starting + page_size
+
+    return starting, end
