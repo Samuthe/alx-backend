749c38549a283ad70ecb4346138087be682a2a83
Samuthe <ekatisamuel@gmail.com>
2023-03-01 16:26:34 +0300

    Basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,5 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def get_index() -> str:
+    """returns home/index page
+   """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
 babel = Babel(app)
