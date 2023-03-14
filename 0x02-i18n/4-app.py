055503ec75b68c64b98f212365cb2e0f37a1df14
Samuthe <ekatisamuel@gmail.com>
2023-03-14 20:03:27 +0300

    3-app

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+'''Module app'''
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    '''i18n config'''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAUL_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    '''render html page'''
+    return render_template('4-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    '''gets best fmatch locale according to request'''
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='5000')
