1a43758e9d9a835d97cbd4ee607ad64aa2c9f1d9
Samuthe <ekatisamuel@gmail.com>
2023-03-15 15:23:37 +0300

    4-app

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,51 +1,89 @@
 #!/usr/bin/env python3
-"""_defining languages_
-Returns:
-    _type_: flask application
+# """_defining languages_
+# Returns:
+#     _type_: flask application
+# """
+# from flask_babel import Babel, _
+# from flask import render_template, Flask, request
+
+
+# class Config:
+#     """_config file for flask_bable_
+#     """
+#     BABEL_DEFAULT_TIMEZONE = 'UTC'
+#     BABEL_DEFAULT_LOCALE = 'en'
+#     LANGUAGES = ["en", "fr"]
+
+
+# app = Flask(__name__)
+# app.config.from_object(Config)
+# babel = Babel(app)
+# '''
+# app configs and calls
+# '''
+
+
+# @babel.localeselector
+# def get_locale():
+#     """_Language Localisation_
+#     Returns:
+#         Str: prefered user language
+#     """
+#     try:
+#         language = [i for i in Config.LANGUAGES]
+#     except ValueError:
+#         language = None
+#     if language is not None:
+#         for j in language:
+#             locale = request.args.get('locale')
+#             if locale == j:
+#                 return locale
+#     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+# @app.route('/')
+# @app.route('/templates/4-index.html')
+# def index() -> str:
+#     """_Page Render_
+#     Returns:
+#         str: Content of html
+#     """
+#     return render_template('4-index.html')
+
+
 """
-from flask_babel import Babel, _
-from flask import render_template, Flask, request
+Module 0-app
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
 
 
-class Config:
-    """_config file for flask_bable_
-    """
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+class Config(object):
+    """i18n configuration"""
+    LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
-    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
-'''
-app configs and calls
-'''
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """didplays a basic hello world message"""
+    return render_template("4-index.html")
 
 
 @babel.localeselector
 def get_locale():
-    """_Language Localisation_
-    Returns:
-        Str: prefered user language
-    """
-    try:
-        language = [i for i in Config.LANGUAGES]
-    except ValueError:
-        language = None
-    if language is not None:
-        for j in language:
-            locale = request.args.get('locale')
-            if locale == j:
-                return locale
+    """Gets best fmatch locale according to request"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
-@app.route('/templates/4-index.html')
-def index() -> str:
-    """_Page Render_
-    Returns:
-        str: Content of html
-    """
-    return render_template('4-index.html')
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port="5000")
